cddd3bf69f1bea2eac42f3d3b81ff73d4ca5a634
chinganc <chinganc0@gmail.com>
2024-06-27 09:45:54 -0700

    Add missing optimizers dir

---
+++ opto/optimizers/__init__.py
@@ -0,0 +1,6 @@
+from opto.optimizers.function_optimizer import (
+    FunctionOptimizer,
+    FunctionOptimizerV2,
+    FunctionOptimizerV2Memory,
+)
+from opto.optimizers.opro import OPRO
---
+++ opto/optimizers/buffers.py
@@ -0,0 +1,16 @@
+class FIFOBuffer:
+    # A basic FIFO buffer
+    def __init__(self, size: int):
+        self.size = size
+        self.buffer = []
+
+    def add(self, item):
+        if self.size > 0:
+            self.buffer.append(item)
+            self.buffer = self.buffer[-self.size :]
+
+    def __iter__(self):
+        return iter(self.buffer)
+
+    def __len__(self):
+        return len(self.buffer)
