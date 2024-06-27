63efb1c04490d948e6ba0cc77de1025ba2c41fb5
chinganc <chinganc0@gmail.com>
2024-06-27 13:36:35 -0700

    Fix a bug in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
 import setuptools
 
-
 install_requires = [
     "pyautogen@git+https://github.com/microsoft/autogen.git@57ec13c#egg=autogen",
     "graphviz==0.20.1",
@@ -17,7 +16,7 @@ setuptools.setup(
     license='MIT LICENSE',
     description="An AutoDiff-like tool for training AI systems end-to-end with general feedback",
     long_description=open('README.md').read(),
-    packages=setuptools.find_packages(include=["opto"], exclude=["test"]),
+    packages=setuptools.find_packages(include=["opto*"], exclude=["test*"]),
     install_requires=install_requires,
     python_requires=">=3.8",
 )
