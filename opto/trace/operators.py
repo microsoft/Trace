ac9196f9cab1ef058cf10624f36edbe9ad533878
chinganc <chinganc0@gmail.com>
2024-07-01 10:54:37 -0700

    Make bundle work with nonlocal settings.

--- opto/trace/operators.py
+++ opto/trace/operators.py
@@ -425,7 +425,7 @@ def format(x: Any, *args, **kwargs):
     """ Fills in a string template with content, str.format(). """
     return x.format(*args, **kwargs)
 
-
+@bundle()
 def node_getattr(obj: Node, attr: str):
     """ This operator gets attr of obj. """
     """ This operator gets attr of obj. """
