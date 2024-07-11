72a767f4268412a1ec4d3f726cb9a8528a84a820
windweller <leo.niecn@gmail.com>
2024-07-10 18:18:15 -0700

    Moved Seq/Map from modules to containers, and added a unit test file for containers

--- opto/trace/modules.py
+++ opto/trace/modules.py
@@ -1,5 +1,4 @@
 from typing import List, Union, Dict, Any
-from collections import UserDict, UserList
 from opto.trace.nodes import Node, ParameterNode
 import inspect
 import functools
@@ -113,62 +112,3 @@ class Module(ParameterContainer):
             else:  # if the parameter does not exist
                 assert k not in self.__dict__
                 setattr(self, k, v)
-
-
-class Seq(UserList, ParameterContainer):
-    """
-    Seq is defined as having a length and an index.
-    Python's list/tuple will be converted to Seq
-    """
-
-    def __init__(self, *args):
-        if len(args) == 1 and hasattr(args[0], "__len__") and hasattr(args[0], "__getitem__"):
-            seq = args[0]
-        else:
-            seq = args
-        super().__init__(initlist=seq)
-
-    def parameters_dict(self):
-        """ Return a dictionary of all the parameters in the model, including
-        both trainable and non-trainable parameters. The dict contains
-        ParameterNodes or ParameterContainers.
-        """
-        parameters = {}
-        for attr in self.data:
-            if isinstance(attr, ParameterNode):
-                parameters[attr.name] = attr
-            elif isinstance(attr, ParameterContainer):
-                parameters[str(attr)] = attr  # TODO: what is the name of the container?
-
-        assert all(isinstance(v, (ParameterNode, ParameterContainer)) for v in parameters.values())
-        return parameters
-
-
-class Map(UserDict, ParameterContainer):
-    """
-    Map is defined as key and value
-    Python's dict will be converted to Map
-    """
-
-    def __init__(self, mapping):
-        super().__init__(mapping)
-
-    def parameters_dict(self):
-        """ Return a dictionary of all the parameters in the model, including
-        both trainable and non-trainable parameters. The dict contains
-        ParameterNodes or ParameterContainers.
-        """
-        parameters = {}
-        for k, v in self.data.items():
-            if isinstance(v, ParameterNode):
-                parameters[k] = v
-            elif isinstance(v, ParameterContainer):
-                parameters[str(v)] = v  # TODO: what is the name of the container?
-
-            if isinstance(k, ParameterNode):
-                parameters[str(k)] = k
-            elif isinstance(k, ParameterContainer):
-                raise Exception("The key of a Map cannot be a container.")
-
-        assert all(isinstance(v, (ParameterNode, ParameterContainer)) for v in parameters.values())
-        return parameters
--- tests/unit_tests/test_modules.py
+++ tests/unit_tests/test_modules.py
@@ -1,4 +1,4 @@
-from opto.trace.modules import Module, model, Map, Seq
+from opto.trace.modules import Module, model
 from opto.trace.nodes import node
 from opto.trace.bundle import bundle
 import os
@@ -127,43 +127,3 @@ except AttributeError:
 # assert child2._extra_method.parameter._data == "fake method"
 # assert child2._base._param._data == 3
 # assert child2._new_param == 1 # simulate new parameter
-
-# test if List/Dict/Tuple type ParameterContainer can be pickled and loaded
-a = Map({"a": 1, "b": 2})
-pickle.dump(a, open("test.pkl", "wb"))
-b = pickle.load(open("test.pkl", "rb"))
-os.remove("test.pkl")
-assert a == b
-
-a = Seq([1, 2, 3])
-pickle.dump(a, open("test.pkl", "wb"))
-b = pickle.load(open("test.pkl", "rb"))
-os.remove("test.pkl")
-assert a == b
-
-a = Map({"a": 1, "b": node(2)})
-pickle.dump(a, open("test.pkl", "wb"))
-b = pickle.load(open("test.pkl", "rb"))
-os.remove("test.pkl")
-assert a == b
-
-a = Seq([1, 2, node(3)])
-pickle.dump(a, open("test.pkl", "wb"))
-b = pickle.load(open("test.pkl", "rb"))
-os.remove("test.pkl")
-assert a == b
-
-# test nested parameter retrieval
-a = Seq([1, 2, Seq(3,4,5)])
-assert a.parameters() == [], "Seq itself is not a parameter node"
-
-a = Seq([1, node(2, trainable=True), Seq(3,node(4, trainable=True),5)])
-assert len(a.parameters()) == 2, "Seq contains 2 parameters"
-
-# both key and value could be parameter nodes
-a = Map({"a": 1, "b": node(2, trainable=True), node('c', trainable=True): 3})
-assert len(a.parameters()) == 2, "Map contains 2 parameters"
-
-# mix and match of Seq and Map
-a = Map({"a": 1, "b": node(2, trainable=True), "c": Seq(3,node(4, trainable=True),5)})
-assert len(a.parameters()) == 2, "Map contains 2 parameters"
