19ddfc8a0bc2ec5e8130bedc18b1e35a50662f17
chinganc <chinganc0@gmail.com>
2024-07-24 23:47:07 -0700

    Rename to OptoPrime

--- opto/optimizers/__init__.py
+++ opto/optimizers/__init__.py
@@ -1,4 +1,4 @@
-from opto.optimizers.function_optimizer import FunctionOptimizerV2Memory as OptoPrime
+from opto.optimizers.function_optimizer import OptoPrime
 from opto.optimizers.opro import OPRO
 
 __all__ = ["OPRO", "OptoPrime"]
