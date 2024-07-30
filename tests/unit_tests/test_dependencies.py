b96be5018a60f6cc958b44dbb9562a372911663a
chinganc <chinganc0@gmail.com>
2024-07-30 09:44:29 -0700

    Update dependency test

--- tests/unit_tests/test_dependencies.py
+++ tests/unit_tests/test_dependencies.py
@@ -1,16 +1,8 @@
 from opto.trace import node, bundle
 from opto.trace.utils import contain, sum_feedback
 
-# x = node(1, name="node_x")
-# y = node(2, name="node_y")
-# a = x + y
-# b = x + 1
-# final = a + b
 
-# final.backward(visualize=True)
-# fig = final.backward(visualize=True)
-# fig.view()
-
+# flat
 x = node(1., trainable=True)
 y = node(2.)
 z = x**y + (x*x*x*x) + 0.5
@@ -20,16 +12,13 @@ assert contain(z.parameter_dependencies, x)
 assert not contain(z.parameter_dependencies, y)
 
 
-
-###
-
+### nested
 x = node(1., trainable=True)
 hidden_param = node(-15., trainable=True)
 
 @bundle()
 def inner_function(x):
     return x**2 + hidden_param
-
 
 @bundle(traceable_code=True)
 def outer_function(x):
@@ -47,7 +36,44 @@ assert contain(output.expandable_dependencies, output)
 
 print(x)
 
-output.backward('feedback', visualize=True)
+output.backward('feedback', visualize=True)  # top graph
+
 g = sum_feedback([x])
-sg, fig = g.expand(output, visualize=True)
+fig = g.visualize()
+fig
+
+sg = g.expand(output)
+assert len(sg.graph) == 6
+
+for _, n in sg.graph:
+    print(n)
+    print('-----')
+
 sg.visualize()
+
+
+### nested (ony hidden params)
+x = node(1.)
+hidden_param = node(-15., trainable=True)
+
+@bundle()
+def inner_function(x):
+    return x**2 + hidden_param
+
+@bundle(traceable_code=True)
+def outer_function(x):
+    return inner_function(x) + 1 + hidden_param
+
+
+output = outer_function(x)
+
+assert len(output.parameter_dependencies) == 0
+assert not contain(output.parameter_dependencies, hidden_param)
+assert len(output.expandable_dependencies) == 1
+assert contain(output.expandable_dependencies, output)
+
+output.backward('feedback')  # top graph
+
+tg = sum_feedback([hidden_param])
+tg.visualize()  # this shows the top level graph
+tg.expand(output).visualize()  # this shows the expanded graph
