351c4302ec4c5de5201a312a3aec7c4dd375b66c
wanqiao <wanqiaox@umich.edu>
2024-07-30 22:33:11 -0700

    suffix check

--- exp/poem_numerical.py
+++ exp/poem_numerical.py
@@ -2,7 +2,7 @@ import llfbench as gym
 import autogen
 from opto.trace.nodes import node
 from opto.trace.bundle import bundle
-from opto.optimizers import FunctionOptimizer
+from opto.optimizers import FunctionOptimizer, FunctionOptimizerV2Memory
 from opto.trace.nodes import GRAPH
 from llfbench.agents.llm import make_llm
 from llfbench.agents.basic_ai_agent import BasicAIAgent
@@ -14,13 +14,19 @@ def reformat(program_str: str):
     return dedent(program_str).strip()
 
 # The hierarchical poem generation task
+# Feedback type:
+    # r: reward
+    # hn: hindsight negative
+    # hp: hindsight positive
+    # fn: future negative
+    # fp: future positive
 env = gym.make('llf-poem-NumericalPlanningPoem-v0') # alternative: Tanka; more general: LineSyllableConstrainedPoem
 
 done = False
 cumulative_reward = 0.0
 
 system_prompt = BasicAIAgent.system_prompt
-llm = make_llm("gpt-35-turbo", system_prompt=system_prompt)
+llm = make_llm("gpt-4", system_prompt=system_prompt)
 agent = BasicAIAgent(llm, verbose=True)
 
 @bundle()
@@ -52,8 +58,8 @@ def step(line_syllables, last_line):
 @bundle()
 def act(prompt):
     "This is a function that asks the agent to act based on the prompt"
-    # agent.reset(prompt)
-    # observation, info = env.reset(options={'syllable_thres': [7,7,8], 'context': 0, 'feedback': 0})
+    agent.reset(prompt)
+    observation, info = env.reset(options={'syllable_req': [[4,5,4]], 'ends_with': 'flourish', 'context': 4})
     print(observation)
     return agent.act(observation['observation'], observation['feedback'])
 
@@ -61,32 +67,37 @@ GRAPH.clear()
 # The prompt to be optimized
 prompt = node("Can you write me a poem?", trainable=True)
 
-optimizer = FunctionOptimizer(
+optimizer = FunctionOptimizerV2Memory(
                             [prompt], 
-                            config_list=autogen.config_list_from_json("OAI_CONFIG_LIST"),
+                            config_list=autogen.config_list_from_json("OAI_CONFIG_LIST_4O"),
                             )
 optimizer.objective = """You are a helpful assistant that wants to come up with instructions to a student to help
 them write a poem that is satisfactory to a teacher's assignment.
-The student's poem needs to satisfy the requirement of this assignment. """ + optimizer.default_objective
+The student's poem needs to satisfy the requirement of this assignment. 
+You should try to incorporate the feedback into your instruction to the student. 
+It should be made clear to the student what to change minimally to satisfy the assignment. """ + optimizer.default_objective
 
-agent.reset(prompt)
-observation, info = env.reset(options={'syllable_req': [[10,6,5]], 'context': 5, 'feedback': 0, 'starts_with': "Once upon a time"})
+history = [prompt.data]
+info = {}
+while not 'success' in info or not info['success']:
+    breakpoint()
+    action = act(prompt)
+    text = check_suffix(action)
+    paras = check_paragraphs(action)
+    lines = check_lines(text, paras)
+    line_syllables, lines = check_syllables(lines)
+    observation, reward, terminated, truncated, info = step(line_syllables, lines)
+    cumulative_reward += reward
+    done = terminated or truncated
 
-" TODO: A single loop attempt for debugging purposes "
-# action = act(prompt)
-action = "This is a poem with one paragraph.\nThis is a second line.\nThis is a third line.\n"
-text = check_suffix(action)
-paras = check_paragraphs(action)
-lines = check_lines(text, paras)
-line_syllables, lines = check_syllables(lines)
-observation, reward, terminated, truncated, info = step(line_syllables, lines)
-cumulative_reward += reward
-done = terminated or truncated
-
-optimizer.zero_feedback()
-optimizer.backward(observation, observation['feedback'], visualize=True)
-print(f"prompt: {prompt.data}, output: {observation['observation']}, feedback: {observation['feedback']}")
-optimizer.step(verbose=True)
+    optimizer.zero_feedback()
+    optimizer.backward(observation, observation['feedback'], visualize=True)
+    print(f"prompt: {prompt.data}, output: {observation['observation']}, feedback: {observation['feedback']}")
+    optimizer.step(verbose=True)
+    history.append(prompt.data)
 
 print(f'Episode reward: {cumulative_reward}')
 print(f'Final prompt: {prompt.data}')
+print('History')
+for i, h in enumerate(history):
+    print(f'{i}: {h}')
