4ca8641ed08714717da89ce37a064a39d022883f
wanqiao <wanqiaox@umich.edu>
2024-07-31 17:40:33 -0700

    fix merge error

--- exp/run_poem.sh
+++ exp/run_poem.sh
@@ -1,2 +1,2 @@
-python poem_exp.py exp_name=poem wandb.enabled=True debug=False
-python poem_exp.py exp_name=poem wandb.enabled=True debug=False
+python exp/poem_exp.py exp_name=poem wandb.enabled=True debug=False
+python exp/poem_exp.py exp_name=poem wandb.enabled=True debug=False
