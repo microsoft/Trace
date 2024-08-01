c691735cd68a2b44614295da40cfcc6bc26686f4
wanqiao <wanqiaox@umich.edu>
2024-07-31 18:13:12 -0700

    Revert "batch script"
    This reverts commit 38f75633a3ad320688a6573fc4b6396e94159430.

--- exp/run_poem.sh
+++ exp/run_poem.sh
@@ -1,1 +1,16 @@
-python exp/poem_exp.py exp_name=poem wandb.enabled=True debug=False
+#!/bin/bash
+#SBATCH --job-name=poem
+#SBATCH --partition=a4-cpu
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+#SBATCH --mem=16G
+#SBATCH --time=00:30:00
+#SBATCH --output=outputs/poem_%j.out
+#SBATCH --error=outputs/poem_%j.err
+
+# source ~/miniconda3/etc/profile.d/conda.sh
+eval "$(conda shell.bash hook)"
+conda activate trace
+export PYTHONPATH=$PWD
+
+python poem_exp.py exp_name=poem 
