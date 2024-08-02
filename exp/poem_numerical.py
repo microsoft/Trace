deb5de194c3f68136ce3287e578025655832a870
wanqiao <wanqiaox@umich.edu>
2024-08-02 10:29:38 -0700

    change feedback to str

--- exp/poem_numerical.py
+++ exp/poem_numerical.py
@@ -92,7 +92,7 @@ def trace_poem_generation(config: PoemConfig, debug: bool = False, wandb_enabled
         optimizer = OptoPrime(
                                 [prompt], 
                                 config_list=autogen.config_list_from_json(OAI_CONFIG_LIST[config.teacher_model]),
-                                memory_size=10,
+                                memory_size=0,
                                 )
         optimizer.objective = """You are a helpful assistant that wants to come up with instructions to a student to help
         them write a poem that is satisfactory to a teacher's assignment.
@@ -123,12 +123,13 @@ def trace_poem_generation(config: PoemConfig, debug: bool = False, wandb_enabled
         observation, reward, terminated, truncated, info = step(line_syllables, lines)
         cumulative_reward += reward
         done = terminated or truncated
+        feedback = observation['feedback'].data
         
         optimizer.zero_feedback()
-        optimizer.backward(observation, observation['feedback'], visualize=True)
-        history[-1].extend([action.data, observation['feedback'].data])
-        breakpoint()
-        if debug: print(f"prompt: {prompt.data}, output: {observation['observation']}, feedback: {observation['feedback']}")
+        optimizer.backward(observation, feedback, visualize=True)
+        history[-1].extend([action.data, feedback])
+
+        if debug: print(f"prompt: {prompt.data}, output: {observation['observation'].data}, feedback: {feedback}")
         if not debug and wandb_enabled:
             wandb.log({'step': len(history), 'reward': float(reward.data), 'cumulative_reward': float(cumulative_reward.data), 'iterations': wandb.Table(data=history, columns=["prompt", "output", "feedback"])})
         optimizer.step(verbose=debug)
