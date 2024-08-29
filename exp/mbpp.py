89a4811386cca0d15825a609e02789c0450bdae7
wanqiao <wanqiaox@umich.edu>
2024-08-29 10:10:58 -0700

    mbpp

--- exp/mbpp.py
+++ exp/mbpp.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 import wandb
 from datasets import load_dataset
 from big_bench_hard import LLMCallable
+import sys, importlib.util
 
 @dataclass
 class MBPPConfig:
@@ -21,241 +22,107 @@ class MBPPConfig:
     cot: bool = False
     load_ckpt: str = ""
     save_path: str = "."
-
-
-def eval_metric(true, prediction, tests):
-    # TODO: Change
-    breakpoint()
-    # use eval() method to evaluate expressions
-    def eval_test(test):
-        return eval(test)
-    # check if the prediction is correct
-    if all([eval_test(test) for test in tests]):
-        return True
-    return False
+    n_optimization_steps: int = 100
     
-
-@model
-class Generate(LLMCallable):
-    # TODO: regulate LLM output to a certain format or let it learn
-    def __init__(self):
-        super().__init__()
-
-        self.demos = []
-        self.prompt_template = dedent(
-            """
-        Given the fields `question`, produce the fields `answer`.
-
-        ---
-
-        Follow the following format.
-
-        Question: 
-        Answer: 
-
-        ---
-        Question: {}
-        Answer:
-        """
-        )
-        self.prompt_template = ParameterNode(self.prompt_template, trainable=True,
-                                             description="This is the Prompt Template to the LLM. " + \
-                                                         "Need to include information about what the format of answers LLM should output. " + \
-                                                         "They can be (A)/(B), a number like 8, or a string, or Yes/No.")
-
-    @bundle(trainable=True, catch_execution_error=True, allow_external_dependencies=True)
-    def extract_answer(self, prompt_template, question, response):
-        answer = response.split("Answer:")[1].strip().split("```python\n")[1].split("\n```")[0]
-        return answer
-
-    @bundle(trainable=True, catch_execution_error=True, allow_external_dependencies=True)
-    def create_prompt(self, prompt_template, question):
-        return prompt_template.format(question)
-
-    @bundle(trainable=True, catch_execution_error=True, allow_external_dependencies=True)
-    def program(self, var1, var2=None, var3=None, var4=None):
-        return var1
-
-    def forward(self, question):
-        user_prompt = self.create_prompt(self.prompt_template, question)
-        response = self.call_llm(user_prompt)
-        answer = self.extract_answer(self.prompt_template, question, response)
-        return answer
-    
-
-@model
-class GenerateCoT(LLMCallable):
-    def __init__(self):
-        super().__init__()
-
-        self.demos = []
-        self.prompt_template = dedent("""
-        Given the fields `question`, produce the fields `answer`.
-
-        ---
-
-        Follow the following format.
-
-        Question: question
-        Reasoning: Let's think step by step in order to produce the answer. We ...
-        Answer: answer
-
-        ---
-        Question: {}
-        """)
-
-        self.prompt_template = ParameterNode(self.prompt_template, trainable=True,
-                                             description="[ParameterNode] This is the Prompt Template to the LLM. " + \
-                                                         "Need to include information about what the format of answers LLM should output. " + \
-                                                         "They can be (A)/(B), a number like 8, or a string, or Yes/No.")
-    
-def learn_generate(dp, optimizer, examples, val_examples, save_dir):
-    cum_reward = 0
-    epochs = 1
-
-    val_perfs = {}
-    # convert test cases to assertions
-    for epoch in range(epochs):
-        for step, example in enumerate(tqdm(examples)):
-            GRAPH.clear()
-            question = example['text']
-            answer = example['code']
-            test_cases = example['test_list']
-            try:
-                breakpoint()
-                response = dp.forward(question)
-                correctness, unsatisfied_tests = eval_metric(response, answer, test_cases)
-                feedback = "The answer is correct! No need to change anything." if correctness else f"The answer is wrong. We expect the output of your answer to satisfy tests \"{unsatisfied_tests.join(',')}\". Please modify the program to produce the right answer."
-                no_error = True
-            except Exception as e:
-                # load in the previous best checkpoint, and try to optimize from that again
-                # an error recovery mode (similar to MCTS!?)
-                if len(val_perfs) > 0:
-                    breakpoint()
-                    best_checkpoint = max(val_perfs, key=val_perfs.get)
-                    dp.load(best_checkpoint)
-                    try:
-                        response = dp.forward(question)
-                        correctness, unsatisfied_tests = eval_metric(response.data, answer, test_cases)
-                        feedback = "The answer is correct! No need to change anything." if correctness else f"The answer is wrong. We expect the output of your answer to satisfy tests \"{unsatisfied_tests.join(',')}\". Please modify the program to produce the right answer."
-                        no_error = True
-                    except:
-                        response = e.exception_node
-                        feedback = response.data
-                        correctness = False
-                        no_error = False
-                else:
-                    response = e.exception_node
-                    feedback = response.data
-                    correctness = False
-                    no_error = False
-
-            print(question)
-            print("Expected answer:", answer)
-            print("Answer:", response.data)
-
-            cum_reward += correctness
-            checkpoint_name = f"{save_dir}/epoch_{epoch}_step_{step}.pkl"
-
-            # if we can handle the case, no need to optimize
-            if correctness:
-                # evaluate on val examples
-                try:
-                    val_perf, _ = evaluate_dp(dp, val_examples)
-                    val_perfs[checkpoint_name] = val_perf
-                    dp.save(checkpoint_name)
-                except:
-                    pass
-
-                continue
-
-            # if val_perf is completely empty and there is no immediate error, we save two checkpoints
-            if no_error and len(val_perfs) < 2:
-                try:
-                    val_perf, _ = evaluate_dp(dp, val_examples)
-                    val_perfs[checkpoint_name] = val_perf
-                    dp.save(checkpoint_name)
-                except:
-                    pass
-
-            optimizer.zero_feedback()
-            optimizer.backward(response, feedback)
-
-            print(f"output={response.data}, feedback={feedback}, variables=\n")  # logging
-            for p in optimizer.parameters:
-                print(p.name, p.data)
-            optimizer.step(verbose=False)
-    # in the end, we select the best checkpoint on validation set
-    # by here we have at least one checkpoint
-    best_checkpoint = max(val_perfs, key=val_perfs.get)
-    print(f"Best checkpoint: {best_checkpoint}", f"Val performance: {val_perfs[best_checkpoint]}")
-    dp.load(best_checkpoint)
-
-    checkpoint_name = f"{save_dir}/best_ckpt.pkl"
-    dp.save(checkpoint_name)
-
-    print(f"Total reward: {cum_reward}")
-    return dp, cum_reward
-
-
-def evaluate_dp(dp, examples):
-    rewards = 0
-    responses = []
-    for example in tqdm(examples):
-        try:
-            response = dp.forward(example["text"])
-            responses.append(response.data)
-            correctness, _ = eval_metric(example["code"], response.data, example["test_list"])
-        except:
-            correctness = False
-            responses.append(None)
-
-        rewards += correctness
-    return rewards / len(examples), responses
-
 
 def mbpp_generation(config: MBPPConfig, debug: bool = False, wandb_enabled: bool = False, optimizer_name: str = 'opto'):
-    save_name = f"./output/"
-    ckpt_save_name = f"./output/mbpp"
-
-    if config.train:
-        save_name += "trained_"
-        ckpt_save_name += "_train"
-    if config.cot:
-        save_name += "cot_"
-        ckpt_save_name += "_cot"
-    save_name += f".pkl"
-
     ds = load_dataset("google-research-datasets/mbpp", "full")
     trainset = ds['train']
-    valset = ds['validation']
-    test_set = ds['test']
-    
-    stats = {}
+    # valset = ds['validation']
+    # test_set = ds['test']
 
-    if config.load_ckpt != "":
-        dp = Generate()
-        dp.load(config.load_ckpt)
-    else:
-        if not config.cot:
-            dp = Generate()
-        else:
-            dp = GenerateCoT()
-        
+    global program
+    @bundle(trainable=True, catch_execution_error=True, allow_external_dependencies=True)
+    def program(var1, var2=None, var3=None, var4=None):
+        """
+        A function that processes inputs into an output.
+        """
+        return var1
+
+
+    @bundle(trainable=False, catch_execution_error=True, allow_external_dependencies=True)
+    def evaluate_test(program, tests):
+        """
+        A function that runs a test on the program. 
+        """
+        func_name = tests[0].replace('assert ', '').split('(')[0]
+        tests = [test.replace(func_name, 'program').replace('assert ', '') for test in tests]
+        # use eval() method to evaluate expressions
+        def eval_test(test):
+            return eval(test)
+        for test in tests:
+            try:
+                if not eval_test(test):
+                    return False, test, None
+            except ExecutionError as e:
+                print(e.exception_node.data)
+                return False, test, e.exception_node
+        return True, None, None
+
+    successes = []
+    returns = []
+    print("Optimization starts")
+    for i, example in enumerate(trainset):
+        if i == 0: continue
+        print("Optimization starts for example", i)
+
+        GRAPH.clear()
         if optimizer_name == 'opto':
-            optimizer = OptoPrime(dp.parameters() + [dp.prompt_template],
-                                    config_list=autogen.config_list_from_json("OAI_CONFIG_LIST"))
+            optimizer = OptoPrime(
+                                    program.parameters(), 
+                                    config_list=autogen.config_list_from_json("OAI_CONFIG_LIST_INT"),
+                                    memory_size=0,
+                                    )
         elif optimizer_name == 'opro':
-            optimizer = OPRO(dp.parameters() + [dp.prompt_template],
-                                    config_list=autogen.config_list_from_json("OAI_CONFIG_LIST"))
+            optimizer = OPRO(
+                                program.parameters(), 
+                                config_list=autogen.config_list_from_json("OAI_CONFIG_LIST_INT"),
+                                )
         elif optimizer_name == 'synth':
-            optimizer = OptoPrime(dp.parameters() + [dp.prompt_template],
-                                    config_list=autogen.config_list_from_json("OAI_CONFIG_LIST"),
+            optimizer = OptoPrime(
+                                    program.parameters(), 
+                                    config_list=autogen.config_list_from_json("OAI_CONFIG_LIST_INT"),
+                                    memory_size=0,
                                     synthesize=True,
                                     wandb_enabled=wandb_enabled and not debug
                                     )
-        dp, rewards = learn_generate(dp, optimizer, trainset, valset, ckpt_save_name)
-        stats['optimizer_log'] = optimizer.log
-        stats['train_acc'] = rewards / len(trainset)
+            
+        
+        question = example["text"]
+        answer = example["code"]
+        test_cases = example["test_list"]
+        steps = 0
+        
+        optimizer.objective = question + optimizer.default_objective
+        for j in range(config.n_optimization_steps):
+            optimizer.zero_feedback()
+            # TODO: hide test cases from the optimizer
+            correctness, unsatisfied_test, error = evaluate_test(program, test_cases)
 
-    
+            if error:
+                feedback = error.data
+            elif correctness:
+                feedback = "The generated program is correct! No need to change anything."
+            else:
+                feedback = f"The answer is wrong. We expect your generated program to satisfy the test \"{unsatisfied_test.data}\". Please modify the program to produce the right answer that passes the test."
+            optimizer.backward(correctness, feedback)
+            optimizer.step(verbose=debug)
+
+            print(f"Step: {steps} code: \n{program.parameters()[0].data}\n feedback: {feedback}")
+            if correctness:
+                successes.append(i)
+                break
+            steps += 1
+        returns.append(-1 * steps)
+        successes.append(correctness)
+        if wandb_enabled and not debug:
+            wandb.log({"steps": steps, "success": correctness, "success rate": sum(successes) / len(successes)})
+        print(f"Example {i} done in {steps} steps")
+
+    print(f"Optimization finished. Success rate: {sum(successes)}/{len(successes)}")
+    print(f"Average steps: {- sum(returns) / len(returns)}")
+    print(f"Max steps: {- min(returns)}")
+    print(f"Min steps: {- max(returns)}")
+    if wandb_enabled and not debug:
+        wandb.finish()
+    return program
