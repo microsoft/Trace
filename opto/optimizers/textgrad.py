98982aabbfb6deec860e6246f7215f0bebfefb64
windweller <leo.niecn@gmail.com>
2024-09-09 10:59:22 -0700

    add textgrad

---
+++ opto/optimizers/textgrad.py
@@ -0,0 +1,159 @@
+from opto.optimizers.optimizer import Optimizer
+
+from textwrap import dedent, indent
+from collections import defaultdict
+
+"""
+Prompts are taken verbatim from:
+https://github.com/zou-group/textgrad/blob/main/textgrad/optimizer/optimizer_prompts.py
+"""
+
+GLOSSARY_TEXT = """
+### Glossary of tags that will be sent to you:
+# - <LM_SYSTEM_PROMPT>: The system prompt for the language model.
+# - <LM_INPUT>: The input to the language model.
+# - <LM_OUTPUT>: The output of the language model.
+# - <FEEDBACK>: The feedback to the variable.
+# - <CONVERSATION>: The conversation history.
+# - <FOCUS>: The focus of the optimization.
+# - <ROLE>: The role description of the variable."""
+
+### Optimize Prompts
+
+# System prompt to TGD
+OPTIMIZER_SYSTEM_PROMPT = (
+    "You are part of an optimization system that improves text (i.e., variable). "
+    "You will be asked to creatively and critically improve prompts, solutions to problems, code, or any other text-based variable. "
+    "You will receive some feedback, and use the feedback to improve the variable. "
+    "The feedback may be noisy, identify what is important and what is correct. "
+    "Pay attention to the role description of the variable, and the context in which it is used. "
+    "This is very important: You MUST give your response by sending the improved variable between {new_variable_start_tag} {{improved variable}} {new_variable_end_tag} tags. "
+    "The text you send between the tags will directly replace the variable.\n\n"
+    f"{GLOSSARY_TEXT}"
+)
+
+# TGD update instruction
+TGD_PROMPT_PREFIX = (
+    "Here is the role of the variable you will improve: <ROLE>{variable_desc}</ROLE>.\n\n"
+    "The variable is the text within the following span: <VARIABLE> {variable_short} </VARIABLE>\n\n"
+    "Here is the context and feedback we got for the variable:\n\n"
+    "<CONTEXT>{variable_grad}</CONTEXT>\n\n"
+    "Improve the variable ({variable_desc}) using the feedback provided in <FEEDBACK> tags.\n"
+)
+
+# If the gradients are in a multi-part container
+TGD_MULTIPART_PROMPT_INIT = (
+    "Here is the role of the variable you will improve: <ROLE>{variable_desc}</ROLE>.\n\n"
+    "The variable is the text within the following span: <VARIABLE> {variable_short} </VARIABLE>\n\n"
+    "Here is the context and feedback we got for the variable:\n\n"
+)
+
+TGD_MULTIPART_PROMPT_PREFIX = (
+    "Improve the variable ({variable_desc}) using the feedback provided in <FEEDBACK> tags.\n"
+)
+
+TGD_PROMPT_SUFFIX = (
+    "Send the improved variable "
+    "in the following format:\n\n{new_variable_start_tag}{{the improved variable}}{new_variable_end_tag}\n\n"
+    "Send ONLY the improved variable between the <IMPROVED_VARIABLE> tags, and nothing else."
+)
+
+MOMENTUM_PROMPT_ADDITION = (
+    "Here are the past iterations of this variable:\n\n"
+    "<PAST_ITERATIONS>{past_values}</PAST_ITERATIONS>\n\n"
+    "Similar feedbacks across different steps suggests that the modifications to the variable are insufficient."
+    "If this is the case, please make more significant changes to the variable.\n\n"
+)
+
+CONSTRAINT_PROMPT_ADDITION = (
+    "You must follow the following constraints:\n\n"
+    "<CONSTRAINTS>{constraint_text}</CONSTRAINTS>\n\n"
+)
+
+IN_CONTEXT_EXAMPLE_PROMPT_ADDITION = (
+    "You must base on the following examples when modifying the {variable_desc}:\n\n"
+    "<EXAMPLES>{in_context_examples}</EXAMPLES>\n\n"
+)
+
+
+def construct_tgd_prompt(do_momentum: bool = False,
+                         do_constrained: bool = False,
+                         do_in_context_examples: bool = False,
+                         **optimizer_kwargs):
+    """
+    Construct the textual gradient descent prompt.
+
+    :param do_momentum: Whether to include momentum in the prompt.
+    :type do_momentum: bool, optional
+    :param do_constrained: Whether to include constraints in the prompt.
+    :type do_constrained: bool, optional
+    :param do_in_context_examples: Whether to include in-context examples in the prompt.
+    :type do_in_context_examples: bool, optional
+    :param optimizer_kwargs: Additional keyword arguments for formatting the prompt. These will be things like the variable description, gradient, past values, constraints, and in-context examples.
+    :return: The TGD update prompt.
+    :rtype: str
+    """
+
+    if isinstance(optimizer_kwargs["variable_grad"], str):
+        multipart = False
+        prompt = TGD_PROMPT_PREFIX.format(**optimizer_kwargs)
+
+    else:
+        gradient_context = optimizer_kwargs["variable_grad"]
+        gradient_context = [TGD_MULTIPART_PROMPT_INIT.format(**optimizer_kwargs)] + gradient_context
+        multipart = True
+        prompt = TGD_MULTIPART_PROMPT_PREFIX.format(**optimizer_kwargs)
+
+    if do_momentum:
+        prompt += MOMENTUM_PROMPT_ADDITION.format(**optimizer_kwargs)
+
+    if do_constrained:
+        prompt += CONSTRAINT_PROMPT_ADDITION.format(**optimizer_kwargs)
+
+    if do_in_context_examples:
+        prompt += IN_CONTEXT_EXAMPLE_PROMPT_ADDITION.format(**optimizer_kwargs)
+
+    prompt += TGD_PROMPT_SUFFIX.format(**optimizer_kwargs)
+
+    if not multipart:
+        return prompt
+
+    else:
+        return gradient_context + [prompt]
+
+
+GRADIENT_TEMPLATE = (
+    "Here is a conversation:\n\n<CONVERSATION>{context}</CONVERSATION>\n\n"
+    "This conversation is potentially part of a larger system. The output is used as {response_desc}\n\n"
+    "Here is the feedback we got for {variable_desc} in the conversation:\n\n<FEEDBACK>{feedback}</FEEDBACK>\n\n"
+)
+GRADIENT_MULTIPART_TEMPLATE = (
+    "Above is a conversation with a language model.\n"
+    "This conversation is potentially part of a larger system. The output is used as {response_desc}\n\n"
+    "Here is the feedback we got for {variable_desc} in the conversation:\n\n<FEEDBACK>{feedback}</FEEDBACK>\n\n"
+)
+
+"""
+Implementation loosely adapted from
+https://github.com/zou-group/textgrad/blob/main/textgrad/optimizer/optimizer.py
+"""
+
+class TextGrad(Optimizer):
+
+    def __init__(self, parameters, *args, **kwargs):
+        super().__init__(parameters, *args, **kwargs)
+
+    def _step(self):
+        trace_graph = self.trace_graph  # aggregate the trace graphes into one.
+        grads = defaultdict(str) # accumulated gradient
+        # trace_graph.graph is a list of nodes sorted according to the topological order
+        for i, (_, x) in enumerate(reversed(trace_graph.graph)):  # back-propagation starts from the last node
+            if len(x.parents) == 0:
+                continue
+            # we take the
+            g = trace_graph.user_feedback if i == 0 else grads[x]
+        for p in self.parameters:
+            if p.trainable:
+                self._velocity[p] = self._momentum * self._velocity[p] - self._learning_rate * p.feedback
+                update_dict[p] = p.data + self._velocity[p]
+        return update_dict
