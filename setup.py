d4cbee97e95005bb3230bb6677f578e42b89dc4d
Adith Swaminathan <aswaminathan@netflix.com>
2025-01-13 09:35:29 -0800

    Fix for installing on Win11 systems

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 here = os.path.abspath(os.path.dirname(__file__))
 version = {}
-with open(os.path.join(here, "opto/version.py")) as fp:
+with open(os.path.join(here, "opto/version.py"), encoding="utf8") as fp:
     exec(fp.read(), version)
 __version__ = version["__version__"]
 
@@ -23,7 +23,7 @@ setuptools.setup(
     url="https://github.com/microsoft/trace",
     license='MIT LICENSE',
     description="An AutoDiff-like tool for training AI systems end-to-end with general feedback",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding="utf8").read(),
     packages=setuptools.find_packages(include=["opto*"]),
     install_requires=install_requires,
     python_requires=">=3.9",
