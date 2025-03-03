522737a198b9dda6b89d2dc796e23a49dcf2f27e
windweller <leo.niecn@gmail.com>
2025-03-03 12:24:59 -0800

    updated async test (not finding the bug)

--- tests/unit_tests/test_async.py
+++ tests/unit_tests/test_async.py
@@ -6,6 +6,10 @@ def add(x):
     y = node(3)
     return x+y
 
+def sub(x):
+    z = node(4)
+    return x - z
+
 N=100
 
 inputs = range(100)
@@ -13,7 +17,16 @@ def nested_add(i):
     time.sleep(10-i/10)
     return add(inputs[i])
 
-scores = async_run([nested_add] * N, [(i,) for i in range(N)])
+def nested_subtract(x, i):
+    time.sleep(i/10)
+    return sub(x)
+
+def run(i):
+    res1 = nested_add(i)
+    res2 = nested_subtract(res1, i)
+    return res2
+
+scores = async_run([run] * N, [(i,) for i in range(N)])
 
 var_names = []
 for i in range(100):
